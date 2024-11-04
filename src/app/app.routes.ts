622145d386e46621ba42d7de41970a2e897b8ed0
david <dperezfernandez@safareyes.es>
2024-11-04 16:22:16 +0100

    ✨ Added exercise 2 to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import {Ejercicio1Component} from '../ejercicio1/ejercicio1.component';
+import {Ejercicio2Component} from '../ejercicio2/ejercicio2.component';
 
 export const routes: Routes = [
   {'path': 'Ejercicio1', 'component': Ejercicio1Component},
+  {'path': 'Ejercicio2', 'component': Ejercicio2Component},
+  {'path': '', 'redirectTo': 'Ejercicio1', 'pathMatch': 'full'},
+  {'path': '**', 'redirectTo': 'Ejercicio1', 'pathMatch': 'full'}
 ];
